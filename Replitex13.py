b2c8a7d27feadbc12bde7ecefe01e9a1f0578da2
VihaanTheGamer <anujksinghal@gmail.com>
2024-08-11 12:29:25 +0530

    add sme more files

---
+++ Replitex13.py
@@ -0,0 +1,12 @@
+#Write your code below this line 👇
+a=float(input("Enter a height:"))
+b=float(input("Enter a width:"))
+c=(int(input("Enter a coverage:")))
+
+d= (a * b) / (c)
+e=round(d)
+print(e)
+#Write your code above this line 👆
+
+# 🚨 Don't change the code below 👇
+
