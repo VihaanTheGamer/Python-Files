7596d8dd7aeef5cccc23e91832826442c305a473
VihaanTheGamer <anujksinghal@gmail.com>
2024-09-08 11:44:41 +0530

    add new files

--- draw_poly.py
+++ draw_poly.py
@@ -7,9 +7,33 @@ vihaan.shape("turtle")
 
 
 def drawpoly(sides,side_length):
-    pass
+    angle=360/sides
+    for i in range(sides):
+
+        vihaan.forward(side_length)
+        vihaan.left(angle)
+
+a=int(input("Enter a number"))
+drawpoly(a, 100)    
+        
+
+def square(side_length):
+    for i in range(4):
+        angle=360/4
+        vihaan.forward(side_length)
+        vihaan.left(angle)
+
+#square(100)        
+        
+        
+        
+    
 
 
 
 
 wn.exitonclick()
+
+
+
+
